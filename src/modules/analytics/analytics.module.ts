f4aabc7402009d1dda0e5c00d353aff832b2fc15
Ingenio <tiendaalmora@gmail.com>
2026-08-25 19:01:15 -0300

    fix(analytics): fix PrismaService provider in AnalyticsModule for clean Docker build

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
-import { PrismaModule } from '../../shared/database/prisma.module';
+import { PrismaService } from '../../shared/database/prisma.service';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [AnalyticsController],
-  providers: [AnalyticsService],
+  providers: [AnalyticsService, PrismaService],
   exports: [AnalyticsService],
 })
 export class AnalyticsModule {}
